90e617dfd3c4134c34094297de8a60188f39aa8d
Alexey <Lehan347@yandex.ru>
2024-07-25 13:56:21 +0300

    Add project files.

---
+++ ConvertJsonToXml/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+using System.Text.Json;
+
+string json = "{\"name\":\"Alexey\",\"age\":39,\"city\":\"Moscow\"}";
+
+JsonDocument jsonDoc = JsonDocument.Parse(json);
+
+XmlWriterSettings settings = new XmlWriterSettings();
+settings.Indent = true;
+
+using (XmlWriter writer = XmlWriter.Create("output.xml", settings))
+{
+    writer.WriteStartDocument();
+    writer.WriteStartElement("root");
+
+    foreach (JsonProperty property in jsonDoc.RootElement.EnumerateObject())
+    {
+        writer.WriteStartElement(property.Name);
+        writer.WriteString(property.Value.ToString());
+        writer.WriteEndElement();
+    }
+
+    writer.WriteEndElement();
+    writer.WriteEndDocument();
+}
